ce6c31d815748d4481a49a58d908a01cfcf421fb
YocelinSantiago30 <Vanne81gtz06@gmail.com>
2024-09-06 22:07:08 -0600

    app para nombre, correo, telefono

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,47 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Principal {
+    private JLabel labNombre;
+    private JTextField txtNombre;
+    private JLabel labAPat;
+    private JTextField txtAPat;
+    private JTextField txtAMat;
+    private JLabel labMaterno;
+    private JLabel labCorreo;
+    private JTextField txtCorreo;
+    private JLabel labTelefono;
+    private JTextField txtTelefono;
+    private JCheckBox chkdiscapacidad;
+    private JButton btnAceptar;
+    private JButton btnCancelar;
+    private JTextField txtCURP;
+
+
+    public Principal() {
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Borrar();
+            }
+        });
+    }
+    private void Borrar()
+    {
+        txtNombre.setText("");
+        txtAPat.setText("");
+        txtAMat.setText("");
+        txtCURP.setText("");
+        txtCorreo.setText("");
+        txtTelefono.setText("");
+        chkdiscapacidad.setSelected(false);
+    }
+
 }
---
+++ src/alumnos/alumno.java
@@ -0,0 +1,19 @@
+package alumnos;
+
+public class alumno {
+    private String nombre;
+    private String apPaterno;
+    private String apMaterno;
+    private String telefono;
+    private String correo;
+
+
+    public alumno(String nombre, String apPaterno, String apMaterno, String telefono, String correo)
+    {
+        this.nombre = nombre;
+        this.apPaterno = apPaterno;
+        this.apMaterno = apMaterno;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+}
